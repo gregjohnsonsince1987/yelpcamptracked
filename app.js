245561e4a13916b06a29559af47b0cc0d14229ee
GregJohnsonSince1987 <gregjohnsonsince1987@gmail.com>
2020-03-23 05:58:20 -0500

    configured the campgrounds POST route

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 
+var campgrounds = [
+    {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1533684232926-d64430f38b98?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+    {name:"Granite Hill", image:"https://images.unsplash.com/photo-1558552709-7c68bf76b9ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+    {name:"Gregory's Bald", image:"https://images.unsplash.com/photo-1466567270060-295a602c8ce1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}
+]
+
+
 //Landing Page
 app.get("/", function(req, res){
     res.render("landing");
@@ -16,11 +23,6 @@ app.get("/", function(req, res){
 
 //Campgrounds Route - GET
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1533684232926-d64430f38b98?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-        {name:"Granite Hill", image:"https://images.unsplash.com/photo-1558552709-7c68bf76b9ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-        {name:"Gregory's Bald", image:"https://images.unsplash.com/photo-1466567270060-295a602c8ce1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}
-    ]
     
     //{Arg2 - This is the name we are giving to the data we are passing through , Arg1 - This is the data we are passing through}
 
@@ -32,8 +34,16 @@ app.get("/campgrounds", function(req, res){
 //Campgrounds Route - POST - Takes information from form and actually creates a new campground
 app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+
+    // push new campground into our array of campgrounds
+    campgrounds.push(newCampground);
+
     // redirect back to campgrounds page
-    res.send("YOU HIT THE POST ROUTE!");
+    res.redirect("/campgrounds");
+
 });
 
 
