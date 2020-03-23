f264dfcd86a687431f64364763a1062b4f4d5eb7
GregJohnsonSince1987 <gregjohnsonsince1987@gmail.com>
2020-03-23 03:30:23 -0500

    Added views/campground.ejs, created GET campgrounds route, display array of campgrounds

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ app.get("/", function(req, res){
 
 
 
+//Campgrounds Route - GET
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1533684232926-d64430f38b98?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name:"Granite Hill", image:"https://images.unsplash.com/photo-1558552709-7c68bf76b9ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+        {name:"Gregory's Bald", image:"https://images.unsplash.com/photo-1466567270060-295a602c8ce1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"}
+    ]
+    
+    //{Arg2 - This is the name we are giving to the data we are passing through , Arg1 - This is the data we are passing through}
+    
+    res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+
 
 app.listen(3000, function(){
 	console.log("YelpCamp Server is now running on port 3000...");
