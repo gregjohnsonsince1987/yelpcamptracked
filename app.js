afc30b0aadf0bfb14c161cd5ef61b3744cf79c5b
GregJohnsonSince1987 <gregjohnsonsince1987@gmail.com>
2020-03-23 03:09:30 -0500

    Created views directory with landing.ejs views inside

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require("express");
 var app = express();
 
 
+app.set("view engine", "ejs");
+
+
 //Landing Page
 app.get("/", function(req, res){
-    res.send("IT WORKS!");
+    res.render("landing");
 });
 
 
